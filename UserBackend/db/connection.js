7f8e0c2ab95014d8b830a515361c9ff94c03c953
kritikakhandelwal08 <114297252+kritikakhandelwal08@users.noreply.github.com>
2025-04-20 16:40:05 +0530

    Update connection.js

--- UserBackend/db/connection.js
+++ UserBackend/db/connection.js
@@ -1,28 +1,10 @@
-// import mongoose from "mongoose"
-
-// const connectDB=()=>{
-//     mongoose.connect("mongodb+srv://vanshita222003:Klawclaw_22@mitrc.onxuacg.mongodb.net/bloging")
-    
-// .then(()=>{
-//     console.log("successfully connected to mongodb")
-// }).catch((e)=>{
-//     console.log(e)
-// })
-// }
-
-
-
-
-
-// export default connectDB
-
 
 import mongoose from "mongoose";
 
 const connectDB = () => {
     // First database connection
     mongoose
-        .connect("mongodb+srv://vanshita222003:Klawclaw_22@mitrc.onxuacg.mongodb.net/bloging")
+        .connect("YOUR MONGODB URL")
         .then(() => {
             console.log("Successfully connected to the first database blogging");
         })
